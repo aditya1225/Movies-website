7f30095a425360730e82b3a2f3a866405808e08b
Aditya Vasisht <adityacvasisht@gmail.com>
2023-12-04 18:24:31 +0530

    First Commit

---
+++ src/main/java/com/aditya/movies/Review.java
@@ -0,0 +1,24 @@
+package com.aditya.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+
+    private String Body;
+
+    public Review(String Body) {
+        //this.id = id;
+        this.Body = Body;
+    }
+}
